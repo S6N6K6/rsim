393e75ccdea16ab44ab48b7d98849a83be7c9714
Calin <rosianu.calin@gmail.com>
2025-12-29 17:34:27 +0200

    Example Layer and usage in the main app

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,9 +2,25 @@
 
 #include "RSIM.h"
 
+class ExampleLayer : public RSIM::Layer {
+  public:
+    ExampleLayer()
+      :RSIM::Layer("Example") {}
+
+  void OnUpdate() override {
+    RSIM_INFO("ExampleLayer::OnUpdate()");
+  }
+
+  void OnEvent(RSIM::Event& e) override {
+    RSIM_TRACE("{0}", static_cast<RSIM::Event&>(e));
+  }
+};
+
 class RSIM_API Sandbox : public RSIM::Application {
   public:
-    Sandbox() {}
+    Sandbox() {
+    PushLayer(new ExampleLayer());
+ }
     ~Sandbox() {}
 };
 
